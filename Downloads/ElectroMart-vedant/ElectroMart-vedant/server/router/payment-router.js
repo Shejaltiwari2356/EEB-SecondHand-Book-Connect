aefaba8a5920692d2a0cd711150d0609c6bde1cf
kaushalcracks <156807627+kaushalcracks@users.noreply.github.com>
2024-10-24 22:14:54 +0530

    Delete Downloads directory

--- Downloads/ElectroMart-vedant/ElectroMart-vedant/server/router/payment-router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createPayment } = require("../controllers/payment-controller"); // Adjust path as needed
-
-// Route to create a payment
-router.post("/payment", createPayment);
-
-module.exports = router;
